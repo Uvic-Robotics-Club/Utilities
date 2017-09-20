a3d9bf7c3d7992c234efba5c2edc3c64b8403f49
Joel Newman <joellnewman@hotmail.com>
2017-09-20 13:41:47 -0700

    Attempt at testing 2 files
    Testing on ./Robotic Arm Class/ Arm and Geometry

--- Testing/test.py
+++ Testing/test.py
@@ -6,9 +6,16 @@ Created on Wed Sep 20 09:55:40 2017
 """
 
 import sys
-import os
-#print subprocess.Popen("echo Hello World", shell=True, stdout=subprocess.PIPE).stdout.read()
-steam = os.popen("pylint './Robotic Arm Class/Arm.py'")
-print steam.read()
+#import os
+#steam = os.popen("pylint './Robotic Arm Class/Arm.py'")
+from pylint.lint import Run
+test_files = ['./Robotic Arm Class/Arm.py','./Robotic Arm Class/Geometry.py']
+for file_under_test in test_files:
+    results = Run([file_under_test], exit=False)
+    print '*'*20
+    if results.linter.stats['global_note'] < 5.0:
+        print "Failed on {} the pylint score was {:.2f}. To pass you must have a score of above 5.".format(file_under_test, results.linter.stats['global_note'])
+        sys.exit(0)
+
 print "END OF TESTING, FORCING A PASSED BUILD"
-sys.exit(0)
+#sys.exit(0)
