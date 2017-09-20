9be1baed542dd18dae6d9f79f368c00d11730e30
Joel Newman <joellnewman@hotmail.com>
2017-09-20 13:22:11 -0700

    Moved apostrophies for linux system
    Still trying to get the integration working

--- Testing/test.py
+++ Testing/test.py
@@ -8,7 +8,7 @@ Created on Wed Sep 20 09:55:40 2017
 import sys
 import os
 #print subprocess.Popen("echo Hello World", shell=True, stdout=subprocess.PIPE).stdout.read()
-steam = os.popen("pylint ../'Robotic Arm Class'/Arm.py")
+steam = os.popen("pylint '../Robotic Arm Class/Arm.py'")
 print steam.read()
 print "END OF TESTING, FORCING A PASSED BUILD"
 sys.exit(0)
