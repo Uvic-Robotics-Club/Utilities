b05f7a7297952ef4c2a0b180a1c5f265ad37bf7a
Joel Newman <Joellnewman@hotmail.com>
2017-09-20 23:22:30 -0700

    Updated to meet PEP8 Standard

--- Robotic Arm Class/FABRIK demo.py
+++ Robotic Arm Class/FABRIK demo.py
@@ -4,92 +4,90 @@ Created on Sat Sep 02 11:21:39 2017
 
 @author: joell
 """
+from math import pi
 import numpy as np
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
 import matplotlib.animation as animation
-
-from math import pi
+from mpl_toolkits.mplot3d import axes3d
 
 from inverseKinematicUtilities import dh, inverseKinematics_fabrik
 
-meters = 1.0
-feet = 0.3048
 #pylint: disable=C0103
-# Initial angles
-# all angles are taken within the plane that is parallel to all the links
-
-q1 = 45.0/360*2*pi   # rotation of the base about the z axis
-q2 = 60.0/360*2*pi   # rotation of first link relative to the base
-q3 = -30.0/360*2*pi   # rotation of the second link relative to the first link
-q4 = -30.0/360*2*pi   # rotation of the third link relative to the second link
-
-l1 = .02	  # z-axis distance from the global xyz frame to the first joint
-		  # this code assumes the global frame of
-		  # reference has its z-axis centered with the
-		  # first joint
-
-l2 = 3.0*feet	 # length of the first link
-l3 = 2.0*feet	 # length of the second link
-l4 = 1.5*feet	 # length of the third link
-
-
-T10 = dh(q1, l1, 0, pi/2.0) # Create transformation matrix from joint 1 to 0
-T21 = dh(q2, 0, l2, 0)   # Create transformation matrix from joint 2 to 1
-T32 = dh(q3, 0, l3, 0)   # Create transformation matrix from joint 3 to 2
-T43 = dh(q4, 0, l4, 0)   # Create transformation matrix from joint 4 to 3
-
-#NOTE: THESE ARE DOT PRODUCTS
-T20 = np.dot(T10, T21)	  # transformation matrix from frame 2 to global 0
-T30 = np.dot(T20, T32)	  # transformation matrix from frame 2 to global 0
-T40 = np.dot(T30, T43)	  # transformation matrix from frame 2 to global 0
-
-
-# Initial position of each joint
-#was accessing each element with x[1:3, 4] -> translated that into X[0:4, 3]
-p_joints = [T10[0:3, 3], T20[0:3, 3], T30[0:3, 3], T40[0:3, 3]]
-
-p_joints = np.transpose(p_joints)
-
-x_joints_o = p_joints[0]
-y_joints_o = p_joints[1]
-z_joints_o = p_joints[2]
-
-x_joints = p_joints[0]
-y_joints = p_joints[1]
-z_joints = p_joints[2]
-
-
-# Time matrix for example helix path
-N_cycles = 3.0
-T = 1.0
-
-dt = T/100.0
-Nt = int(round(N_cycles*T/dt))
-#orignal was t = [0:1:Nt-1]*dt -> translated to range(0, (Nt-1)*dt, dt)
-t = np.array(range(0, (Nt-1), 1))*dt
-
-x = 1.7*np.cos(2*pi*t+pi/8)
-y = 1.7*np.sin(2*pi*t+pi/8)
-z = -.05-.05*t
-
-tol_limit = .005
-
-
-
-# ROBOT FIGURE
-f_2 = plt.figure(2)
-ax = f_2.add_subplot(111, projection = '3d')
-
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-
-plt.show()
 
 def animate(n):
-	global x_joints, y_joints, z_joints
+	"""
+	This function is called by the animation library from matplotlib.
+
+	Parameters
+	----------
+	n : integer
+		index into array so you can step though it
+
+	Returns
+	-------
+	None
+	"""
+
+	meters = 1.0
+	feet = 0.3048
+
+	# Initial angles
+	# all angles are taken within the plane that is parallel to all the links
+
+	q1 = 45.0/360*2*pi   # rotation of the base about the z axis
+	q2 = 60.0/360*2*pi   # rotation of first link relative to the base
+	q3 = -30.0/360*2*pi   # rotation of the second link relative to the first link
+	q4 = -30.0/360*2*pi   # rotation of the third link relative to the second link
+
+	l1 = .02	  # z-axis distance from the global xyz frame to the first joint
+			  # this code assumes the global frame of
+			  # reference has its z-axis centered with the
+			  # first joint
+
+	l2 = 3.0*feet	 # length of the first link
+	l3 = 2.0*feet	 # length of the second link
+	l4 = 1.5*feet	 # length of the third link
+
+
+	T10 = dh(q1, l1, 0, pi/2.0) # Create transformation matrix from joint 1 to 0
+	T21 = dh(q2, 0, l2, 0)   # Create transformation matrix from joint 2 to 1
+	T32 = dh(q3, 0, l3, 0)   # Create transformation matrix from joint 3 to 2
+	T43 = dh(q4, 0, l4, 0)   # Create transformation matrix from joint 4 to 3
+
+	#NOTE: THESE ARE DOT PRODUCTS
+	T20 = np.dot(T10, T21)	  # transformation matrix from frame 2 to global 0
+	T30 = np.dot(T20, T32)	  # transformation matrix from frame 2 to global 0
+	T40 = np.dot(T30, T43)	  # transformation matrix from frame 2 to global 0
+
+
+	# Initial position of each joint
+	#was accessing each element with x[1:3, 4] -> translated that into X[0:4, 3]
+	p_joints = [T10[0:3, 3], T20[0:3, 3], T30[0:3, 3], T40[0:3, 3]]
+
+	p_joints = np.transpose(p_joints)
+
+	x_joints_o = p_joints[0]
+	y_joints_o = p_joints[1]
+	z_joints_o = p_joints[2]
+
+	x_joints = p_joints[0]
+	y_joints = p_joints[1]
+	z_joints = p_joints[2]
+
+	tol_limit = .005
+	# Time matrix for example helix path
+	N_cycles = 3.0
+	T = 1.0
+
+	dt = T/100.0
+	Nt = int(round(N_cycles*T/dt))
+	#orignal was t = [0:1:Nt-1]*dt -> translated to range(0, (Nt-1)*dt, dt)
+	t = np.array(range(0, (Nt-1), 1))*dt
+
+	x = 1.7*np.cos(2*pi*t+pi/8)
+	y = 1.7*np.sin(2*pi*t+pi/8)
+	z = -.05-.05*t
 
 	# Call Inverse Kinematics function to solve for joint angles
 	[q1, q2, q3, q4, _, _] = inverseKinematics_fabrik(l1, l2, l3, l4,
@@ -151,4 +149,21 @@ def animate(n):
 	plt.hold(False)
 	plt.show()
 
-ani = animation.FuncAnimation(f_2, animate, range(1, Nt-1), interval = 10)
+# ROBOT FIGURE
+Figure = plt.figure(2)
+ax = Figure.add_subplot(111, projection='3d')
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+plt.show()
+tol_limit = .005
+# Time matrix for example helix path
+N_cycles = 3.0
+T = 1.0
+
+dt = T/100.0
+Nt = int(round(N_cycles*T/dt))
+
+ani=animation.FuncAnimation(Figure, animate, range(1, Nt-1), interval = 10)
